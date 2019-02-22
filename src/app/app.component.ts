baaed6b28447142d290cd38235ae0d234304c33b
Pascal Uhe <pascal.uhe@weltbild.com>
2019-02-22 14:13:59 +0100

    browser-back support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSidenav} from '@angular/material';
 import {SidenavService} from './shared/sidenav.service';
-import {Router} from '@angular/router';
+import {Location} from "@angular/common";
 
 @Component({
-  selector: 'root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'earth';
-  @ViewChild('sidenav') sidenav: MatSidenav;
-  constructor(private sidenavService: SidenavService, private router: Router) {}
+    title = 'earth';
+    @ViewChild('sidenav') sidenav: MatSidenav;
 
-  ngOnInit(): void {
-    this.sidenavService.setSidenav(this.sidenav);
-    // this.sidenav.closedStart.subscribe(() => {
-    //   this.router.navigate(['']);
-    // });
-  }
+    constructor(private sidenavService: SidenavService, private location: Location) {
+    }
+
+    ngOnInit(): void {
+        this.sidenavService.setSidenav(this.sidenav);
+
+        this.location.subscribe(() => {
+            if (this.sidenav.opened) {
+                this.sidenavService.close();
+            }
+        });
+    }
 }
